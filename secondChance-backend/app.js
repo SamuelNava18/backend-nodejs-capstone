71fb36b105fd667c4ca96aa453ea4c8068455f44
SamuelNava18 <98237305+SamuelNava18@users.noreply.github.com>
2026-02-08 21:29:03 -0600

    Fix formatting and consistency in app.js

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -13,7 +13,7 @@ const port = 3060
 
 // Connect to MongoDB we just do this one time
 connectToDatabase().then(() => {
-    pinoLogger.info('Connected to DB')
+  pinoLogger.info('Connected to DB')
 }).catch((e) => console.error('Failed to connect to DB', e))
 
 app.use(express.json())
@@ -35,14 +35,14 @@ app.use('/api/secondchance/search', searchRoutes)
 
 // Global Error Handler
 app.use((err, req, res, next) => {
-    console.error(err)
-    res.status(500).send('Internal Server Error')
+  console.error(err)
+  res.status(500).send('Internal Server Error')
 })
 
-app.get("/", (req, res)=>{
-    res.send("Inside the server")
+app.get('/', (req, res)=>{
+  res.send('Inside the server')
 })
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
+  console.log(`Server running on port ${port}`)
 })
