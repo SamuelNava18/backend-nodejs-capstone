1454561d5b8321c33046c8823fd94954bc0ce777
SamuelNava18 <brayan.briones@alumnos.udg.mx>
2026-02-08 21:22:25 -0500

    /login

--- secondChance-backend/routes/authRoutes.js
+++ secondChance-backend/routes/authRoutes.js
@@ -11,7 +11,7 @@ router.post('/register', async (req, res) => {
         // Task 1: Connect to `secondChance` in MongoDB through `connectToDatabase` in `db.js`.
         const db = await connectToDatabase();
         // Task 2: Access MongoDB `users` collection
-        const collection = db.collection(process.env.MONGO_COLLECTION);
+        const collection = db.collection('users');
         // Task 3: Check if user credentials already exists in the database and throw an error if they do
         const existingEmail = await collection.findOne({ email: req.body.email });
 
@@ -45,6 +45,45 @@ router.post('/register', async (req, res) => {
         res.json({ authtoken,email });
     } catch (e) {
          return res.status(500).send('Internal server error');
+    }
+});
+
+router.post('/login', async (req, res) => {
+    try {
+        // Task 1: Connect to `secondChance` in MongoDB through `connectToDatabase` in `db.js`.
+        const db = await connectToDatabase();
+        // Task 2: Access MongoDB `users` collection
+        const collection = db.collection('users');
+        // Task 3: Check for user credentials in database
+        const theUser = await collection.findOne({ email: req.body.email });
+        // Task 4: Check if the password matches the encrypted password and send appropriate message on mismatch
+        if (theUser) {
+            let result = await bcryptjs.compare(req.body.password, theUser.password)
+            if(!result) {
+                logger.error('Passwords do not match');
+                return res.status(404).json({ error: 'Wrong pasword' });
+            }
+            // Task 5: Fetch user details from a database
+            const userName = theUser.firstName;
+            const userEmail = theUser.email;
+            // Task 6: Create JWT authentication if passwords match with user._id as payload
+            let payload = {
+                user: {
+                    id: theUser._id.toString(),
+                 },
+             };
+            const authtoken = jwt.sign(payload, JWT_SECRET)
+            logger.info('User logged in successfully');
+            res.json({authtoken, userName, userEmail });
+        }
+        else {
+            logger.error('User not found');
+            return res.status(404).json({ error: 'User not found' });
+        }
+        // Task 7: Send appropriate message if the user is not found
+    } catch (e) {
+         return res.status(500).send('Internal server error');
+
     }
 });
 module.exports = router;
